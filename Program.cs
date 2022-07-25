2ffc82fc68eb0997cd1b81a0071cd408254aa533
yung-timz <ogungbayitimilehin3@gmail.com>
2022-07-25 08:14:27 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first git project");
+        }
+    }
+}
